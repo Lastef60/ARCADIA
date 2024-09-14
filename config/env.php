0dda03d046956e1ccdcc1fb930d22ffffac8648a
Lastef60 <97059844+Lastef60@users.noreply.github.com>
2024-09-14 14:29:44 +0200

    modification du fichier env.php

---
+++ config/env.php
@@ -0,0 +1,10 @@
+<?php
+
+define('DB_ARCADIA_HOST', getenv('DB_ARCADIA_HOST'));
+define('DB_ARCADIA_PORT', getenv('DB_ARCADIA_PORT'));
+define('DB_ARCADIA_NAME', getenv('DB_ARCADIA_NAME'));
+define('DB_ARCADIA_USER', getenv('DB_ARCADIA_USER'));
+define('DB_ARCADIA_PASSWORD', getenv('DB_ARCADIA_PASSWORD'));
+
+define('DB_MONGO_HOST', getenv('DB_MONGO_HOST'));
+define('DB_MONGO_PORT', getenv('DB_MONGO_PORT'));
